cf8da8fcbf158d23426d58fa4de6d0e4d31625c8
Scripsay <Askell69@yandex.ru>
2016-11-23 15:32:40 +0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php
+$name = 'Алексей';
+$age = 24;
+$mail = 'askell69@mail.ru';
+$town = 'Самара';
+$about = 'Экономист - матетематик';
+echo $name;
+echo $age;
+echo $mail;
+echo $town;
+echo $about;
