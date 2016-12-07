810650c6cd4e9767fdf4dc2fb379ece4eef86222
Scripsay <Askell69@yandex.ru>
2016-12-07 11:44:07 +0400

    Update functionphone.php

--- Json/functionphone.php
+++ Json/functionphone.php
@@ -1,10 +1,7 @@
 <?php
 define('DATA_JSON_FILE', __DIR__ . '/phonebook.json');
 
-/**
- * Возвращает массив данных буджетов
- * @return array|mixed
- */
+
 function getRecords() {
     if (!file_exists(DATA_JSON_FILE)) {
         return [];
@@ -16,11 +13,7 @@ function getRecords() {
     }
     return [];
 }
-/**
- * Поиск предметов по бюджетам
- * @param $what
- * @return array|mixed
- */
+
 function searchWhatItems($what)
 {
     $data = getRecords();
@@ -35,19 +28,9 @@ function searchWhatItems($what)
     }
     return $result;
 }
-/**
- * Получает данные $_GET переменных
- * @param string $name Имя параметра
- * @param null $defaultValue
- * @return null|string
- */
+
 function getQueryParam($name, $defaultValue = null)
 {
     return isset($_GET[$name]) ? $_GET[$name] : $defaultValue;
-    /*if (isset($_GET['what'])) {
-        $whatQuery = $_GET['what'];
-    } else {
-        $whatQuery = null;
-    }
-    return $whatQuery;*/
+    
 }
