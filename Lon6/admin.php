7c230c3e25512a76b2f387e212a0dcdfc7fb9ed7
Scripsay <Askell69@yandex.ru>
2016-12-14 16:07:21 +0400

    Add files via upload

---
+++ Lon6/admin.php
@@ -0,0 +1,37 @@
+<?php
+    include 'functions.php';
+ if (isPOST()) {
+        /*   if (getUploadedFileClientName()) {
+            echo 'Разрешено загружать только Json';
+            die;
+        }*/
+        // Загружаем в нужную папку аватарку
+        if(!uploadFile('test', getUploadedFileNewName())) {
+            echo 'Файл не смог загрузиться';
+            die;
+        }
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Загрузка теста на сервер</title>
+</head>
+<body>
+<form method="POST" enctype="multipart/form-data">
+    <? if(isGET()): ?>
+    <div>
+        <label for="name">Тест </label>
+        <input type="file" id="test" name="test"/>
+    </div>
+    <button type="submit">Отправить</button>
+    <? else: ?>
+        Здравствуйте, тесты загружены <br/>
+    <? endif; ?>
+</form>
+</body>
+</html>
