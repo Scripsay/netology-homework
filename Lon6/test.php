b13b532cf930bbc025273a21f7e9ba0c2a41882f
Scripsay <Askell69@yandex.ru>
2016-12-15 01:37:15 +0400

    Add files via upload

--- Lon6/test.php
+++ Lon6/test.php
@@ -5,7 +5,7 @@
     <title>Загрузка теста на сервер</title>
 </head>
 <body>
-  <form method="get" action="test.php">
+  <form method="GET" action="test.php">
 <?php
 include 'functions.php';
 /* if (isPOST()) {
@@ -21,25 +21,35 @@ $answer = filter_input($type,'answer0');
 $testId = filter_input($type, 'testId');
 $quest = [];
     $data = json_decode(file_get_contents($testId.'.json'), true);
-    if ($answer != null) {
-      echo "bla";
-    } else {
     $id = 0;
-    foreach ($data as $quest) {
-        echo $quest['question'] ?>
-      <input type="text" name="answer" placeholder="введите ответ" id="answer<?php echo $id;?>"></br>
-      <?php $id++?>
+    if ($answer != null) {
+        foreach ($data as $quest) {
+            $answerTmp = filter_input($type, 'answer'.$id);
+            echo $quest['question']." ";
+            if ($answerTmp === strval($quest['correct_answer'])) {
+              echo "Yes!";
+            } else {
+              echo "No(";
+            }
+            echo "<br>"; 
+            $id++;
+            #echo $quest['correct_answer'].'<br>' ;
+        }
+    } else {
+      foreach ($data as $quest) {
+          echo $quest['question'] ?>
+        <input type="text" name="answer<?php echo $id;?>" placeholder="введите ответ"></br>
+        <?php $id++?>
 <?php
     }
-  }
+}
   #  foreach ($data as $value) {
   #    echo $value['correct_answer'];
 #  }
 ?>
 
 <?php if (isGET()): ?>
-
-        <input type="hidden" id="testId" value="<?php echo  $testId.'.json'?>">
+      <input type="hidden" name="testId" value="<?php echo  $testId ?>">
 
         <input type="submit">
   </form>
